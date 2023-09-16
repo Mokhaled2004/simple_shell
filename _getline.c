d58cacfeae79f652c20140bb6f6b38b0cb1b5442
mazenwaelx <mazenwael575@gmail.com>
2023-09-16 15:43:21 -0700

    lol

--- _getline.c
+++ _getline.c
@@ -1,67 +1,74 @@
 #include "shell.h"
 #include <unistd.h>
-
-void *_realloc(void* ptr, size_t old_size, size_t new_size) {
-    void *new_ptr = malloc(new_size);
-    	if (new_size == 0) {
-        free(ptr);
-        return NULL;
-    }
-
-    if (new_ptr == NULL) {
-        return NULL;
-    }
-
-    if (ptr != NULL) {
-        size_t copy_size = old_size;
-        if (copy_size > new_size) {
-            copy_size = new_size;
-        }
-        memcpy(new_ptr, ptr, copy_size);
-
-        free(ptr);
-    }
-
-    return new_ptr;
+/**
+ * _realloc - lol
+ * @ptr: lol
+ * @old_size: loll
+ * @new_size: loolo
+ * Return: lol
+*/
+void *_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+void *new_ptr = malloc(new_size);
+if (new_size == 0)
+{
+free(ptr);
+return (NULL);
 }
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+if (ptr != NULL)
+{
+size_t copy_size = old_size;
+if (copy_size > new_size)
+{
+copy_size = new_size;
+}
+memcpy(new_ptr, ptr, copy_size);
+free(ptr);
+}
+return (new_ptr);
+}
+/**
+ * _getline - lol
+ * @line: loll
+ * @len: loolo
+ * Return: lol
+*/
 int _getline(char **line, int *len)
 {
-	char buffer[1024];
-	int buffer_index = 0;
-	int buffer_size = 0;
-	char c;
+char buffer[1024];
+int buffer_index = 0;
+int buffer_size = 0;
+char c;
 
-	if (line == NULL)
-		return (-1);
+if (line == NULL)
+return (-1);
+while (1)
+{
+if (buffer_index >= buffer_size)
+{
+buffer_size = read(STDIN_FILENO, buffer, 1024);
+buffer_index = 0;
+if (buffer_size <= 0)
+{
+break;
+}
+}
+c = buffer[buffer_index++];
 
-	while (1)
-	{
-		/* If the buffer is empty, read more input */
-		if (buffer_index >= buffer_size)
-		{
-			buffer_size = read(STDIN_FILENO, buffer, 1024);
-			buffer_index = 0;
-			if (buffer_size <= 0)
-			{
-				break;
-			}
-		}
-
-		/* Read a character from the buffer */
-		c = buffer[buffer_index++];
-
-		if (c == '\n')
-		{
-			/* End of line, terminate the string */
-			(*line)[*len] = '\0';
-			break;
-		}
-		else
-		{
-			*line = _realloc(*line, (*len) * sizeof(char), (*len + 1) * sizeof(char));
-			(*line)[(*len)++] = c;
-		}
-	}
-
-	return (*len);
+if (c == '\n')
+{
+(*line)[*len] = '\0';
+break;
+}
+else
+{
+*line = _realloc(*line, (*len) * sizeof(char), (*len + 1) * sizeof(char));
+(*line)[(*len)++] = c;
+}
+}
+return (*len);
 }
--- main.c
+++ main.c
@@ -8,11 +8,11 @@ void execute_command(char *command, char *args[]);
 */
 void print_environmentt(void)
 {
-    int i;
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        printf("%s\n", environ[i]);
-    }
+int i;
+for (i = 0; environ[i] != NULL; i++)
+{
+printf("%s\n", environ[i]);
+}
 }
 /**
  * change_directory - Print the environment variables
@@ -20,33 +20,32 @@ void print_environmentt(void)
 */
 void change_directory(char *directory)
 {
-    char previous_directory[MAX_LINE_LENGTH];
-    previous_directory[0] = '\0';
-
-    if (directory == NULL)
-    {
-        chdir(getenv("HOME"));
-    }
-    else
-    {
-        if (strcmp(directory, "-") == 0)
-        {
-            if (previous_directory[0] != '\0')
-            {
-                printf("%s\n", previous_directory);
-                chdir(previous_directory);
-            }
-            else
-            {
-                printf("No previous directory available.\n");
-            }
-        }
-        else
-        {
-            getcwd(previous_directory, sizeof(previous_directory));
-            chdir(directory);
-        }
-    }
+char previous_directory[MAX_LINE_LENGTH];
+previous_directory[0] = '\0';
+if (directory == NULL)
+{
+chdir(getenv("HOME"));
+}
+else
+{
+if (strcmp(directory, "-") == 0)
+{
+if (previous_directory[0] != '\0')
+{
+printf("%s\n", previous_directory);
+chdir(previous_directory);
+}
+else
+{
+printf("No previous directory available.\n");
+}
+}
+else
+{
+getcwd(previous_directory, sizeof(previous_directory));
+chdir(directory);
+}
+}
 }
 /**
  * execute_command - Print the environment variables
@@ -55,25 +54,25 @@ void change_directory(char *directory)
 */
 void execute_command(char *command, char *args[])
 {
-    pid_t pid = fork();
-    int status;
-    if (pid < 0)
-    {
-        perror("fork failed");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        char command_path[256];
-        snprintf(command_path, sizeof(command_path), "/usr/bin/%s", command);
-        execve(command_path, args, environ);
-        perror("execve failed");
-        exit(1);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+pid_t pid = fork();
+int status;
+if (pid < 0)
+{
+perror("fork failed");
+exit(1);
+}
+else if (pid == 0)
+{
+char command_path[256];
+snprintf(command_path, sizeof(command_path), "/usr/bin/%s", command);
+execve(command_path, args, environ);
+perror("execve failed");
+exit(1);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
 }
 /**
  * main - Simple shell program
@@ -82,57 +81,54 @@ void execute_command(char *command, char *args[])
  */
 int main(void)
 {
-    char *line = NULL, *args[MAX_ARGS];
-    size_t line_length = 0;
-    ssize_t read;
-    int i;
+char *line = NULL, *args[MAX_ARGS];
+size_t line_length = 0;
+ssize_t read;
+int i;
 
-    while (1)
-    {
-        printf("HOME$ ");
-        read = getline(&line, &line_length, stdin);
-        if (read == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("[Disconnected...]\n");
-                exit(0);
-            }
-            else
-            {
-                perror("getline failed");
-                exit(1);
-            }
-        }
+while (1)
+{
+printf("HOME$ ");
+read = getline(&line, &line_length, stdin);
+if (read == -1)
+{
+if (feof(stdin))
+{
+printf("[Disconnected...]\n");
+exit(0);
+}
+else
+{
+perror("getline failed");
+exit(1);
+}
+}
 
-        i = 0;
-        args[i] = strtok(line, " \n");
-        while (args[i] != NULL)
-        {
-            i++;
-            args[i] = strtok(NULL, " \n");
-        }
-        args[i] = NULL;
-
-        if (strcmp(args[0], "cd") == 0)
-        {
-            change_directory(args[1]);
-            continue;
-        }
-        else if (strcmp(args[0], "exit") == 0)
-        {
-            printf("[Disconnected...]\n");
-            exit(0);
-        }
-        else if (strcmp(args[0], "env") == 0)
-        {
-            print_environmentt();
-            continue;
-        }
-
-        execute_command(args[0], args);
-    }
-
-    free(line);
-    return 0;
+i = 0;
+args[i] = strtok(line, " \n");
+while (args[i] != NULL)
+{
+i++;
+args[i] = strtok(NULL, " \n");
+}
+args[i] = NULL;
+if (strcmp(args[0], "cd") == 0)
+{
+change_directory(args[1]);
+continue;
+}
+else if (strcmp(args[0], "exit") == 0)
+{
+printf("[Disconnected...]\n");
+exit(0);
+}
+else if (strcmp(args[0], "env") == 0)
+{
+print_environmentt();
+continue;
+}
+execute_command(args[0], args);
+}
+free(line);
+return (0);
 }
