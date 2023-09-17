7a15b546b544a8f855c7d4e06365349487c3df61
mazenwaelx <mazenwael575@gmail.com>
2023-09-17 06:38:31 -0700

    lol

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
- * print_environmentt - Print the environment variables
- */
-void print_environmentt(void)
-=======
  * print_environment - Print the environment variables
  */
 void print_environment(void)
->>>>>>> a692efbd9c799684a470ac9f79a246e10c8d9267
 {
-    int i;
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        printf("%s\n", environ[i]);
-    }
+int i;
+for (i = 0; environ[i] != NULL; i++)
+{
+printf("%s\n", environ[i]);
+}
 }
 
 /**
@@ -25,32 +19,6 @@ void print_environment(void)
  */
 void change_directory(char *directory, char *previous_directory)
 {
-<<<<<<< HEAD
-    if (directory == NULL)
-    {
-        chdir(getenv("HOME"));
-    }
-    else
-    {
-        if (strcmp(directory, "-") == 0)
-        {
-            if (previous_directory[0] != '\0')
-            {
-                printf("%s\n", previous_directory);
-                chdir(previous_directory);
-            }
-            else
-            {
-                printf("No previous directory available.\n");
-            }
-        }
-        else
-        {
-            getcwd(previous_directory, MAX_LINE_LENGTH);
-            chdir(directory);
-        }
-    }
-=======
 if (directory == NULL)
 {
 chdir(getenv("HOME"));
@@ -75,7 +43,6 @@ getcwd(previous_directory, MAX_LINE_LENGTH);
 chdir(directory);
 }
 }
->>>>>>> a692efbd9c799684a470ac9f79a246e10c8d9267
 }
 
 /**
@@ -85,25 +52,25 @@ chdir(directory);
  */
 void execute_command(char *command, char *args[])
 {
-    pid_t pid = fork();
-    int status;
-    if (pid < 0)
-    {
-        perror("fork failed");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        char command_path[256];
-        snprintf(command_path, sizeof(command_path), "/usr/bin/%s", command);
-        execve(command_path, args, environ);
-        perror("execve failed");
-        exit(1);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+pid_t pid = fork();
+int status;
+if (pid < 0)
+{
+perror("fork failed");
+exit(1);
+}
+else if (pid == 0)
+{
+char command_path[256];
+snprintf(command_path, sizeof(command_path), "/usr/bin/%s", command);
+execve(command_path, args, environ);
+perror("execve failed");
+exit(1);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
 }
 
 /**
@@ -113,48 +80,6 @@ void execute_command(char *command, char *args[])
  */
 int main(void)
 {
-<<<<<<< HEAD
-    char *line = NULL, *args[MAX_ARGS], previous_directory[MAX_LINE_LENGTH];
-    size_t line_length = 0;
-    ssize_t read;
-    int i;
-    previous_directory[0] = '\0';
-
-    while (printf("HOME$ "), (read = getline(&line, &line_length, stdin)) != -1)
-    {
-        i = 0;
-        args[i] = strtok(line, " \n");
-        while (args[i])
-            args[++i] = strtok(NULL, " \n");
-        
-        if (!args[0])
-            continue;
-        if (strcmp(args[0], "cd") == 0)
-        {
-            change_directory(args[1], previous_directory);
-            continue;
-        }
-        if (strcmp(args[0], "exit") == 0)
-        {
-            printf("[Disconnected...]\n");
-            exit(0);
-        }
-        if (strcmp(args[0], "env") == 0)
-        {
-            print_environmentt();
-            continue;
-        }
-
-        execute_command(args[0], args);
-    }
-    if (feof(stdin))
-    {
-        printf("[Disconnected...]\n");
-        exit(0);
-    }
-    perror("getline failed");
-    exit(1);
-=======
 char *line = NULL, *args[MAX_ARGS], previous_directory[MAX_LINE_LENGTH];
 size_t line_length = 0;
 ssize_t read;
@@ -193,5 +118,4 @@ exit(0);
 }
 perror("getline failed");
 exit(1);
->>>>>>> a692efbd9c799684a470ac9f79a246e10c8d9267
 }
