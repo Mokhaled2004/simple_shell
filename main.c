f9b34b3966d512af999faa701f78ca40d9147f17
Mokhaled2004 <mokhaled2004@gmail.com>
2023-09-19 05:59:32 -0700

    Sayonara

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
  */
 void print_environment(void)
 {
-    int i;
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        _printf(environ[i]);
-        _putchar('\n');
-    }
+int i;
+for (i = 0; environ[i] != NULL; i++)
+{
+_printf(environ[i]);
+_putchar('\n');
+}
 }
 
 /**
@@ -20,46 +20,46 @@ void print_environment(void)
  */
 void change_directory(char *directory, char *previous_directory)
 {
-    if (directory == NULL)
-    {
-        chdir(getenv("HOME"));
-    }
-    else
-    {
-        if (_strcmp(directory, "-") == 0)
-        {
-            if (previous_directory[0] != '\0')
-            {
-                _printf(previous_directory);
-                _putchar('\n');
-                chdir(previous_directory);
-            }
-            else
-            {
-                _printf("No previous directory available.\n");
-            }
-        }
-        else
-        {
-            getcwd(previous_directory, MAX_LINE_LENGTH);
-            chdir(directory);
-        }
-    }
+if (directory == NULL)
+{
+chdir(getenv("HOME"));
+}
+else
+{
+if (_strcmp(directory, "-") == 0)
+{
+if (previous_directory[0] != '\0')
+{
+_printf(previous_directory);
+_putchar('\n');
+chdir(previous_directory);
+}
+else
+{
+_printf("No previous directory available.\n");
+}
+}
+else
+{
+getcwd(previous_directory, MAX_LINE_LENGTH);
+chdir(directory);
+}
+}
 }
 
 /**
- * handle_exit - Handle the exit command with the provided exit status
+ * _exitt - Handle the exit command with the provided exit status
  * @args: The arguments for the exit command
  * @exit_status: Reference to the exit status variable
  */
-void handle_exit(char *args[], int *exit_status)
+void _exitt(char *args[], int *exit_status)
 {
-    if (args[1] != NULL)
-    {
-        *exit_status = _atoi(args[1]);
-    }
-    _printf("[Disconnected...]\n");
-    exit(*exit_status);
+if (args[1] != NULL)
+{
+*exit_status = _atoi(args[1]);
+}
+_printf("[Disconnected...]\n");
+exit(*exit_status);
 }
 
 /**
@@ -68,31 +68,32 @@ void handle_exit(char *args[], int *exit_status)
  * @args: The arguments for the command
  * @exit_status: Reference to the exit status variable
  */
-void execute_command(char __attribute__((unused)) *command, char *args[], int *exit_status)
+void execute_command(char __attribute__((unused)) *command,
+char *args[], int *exit_status)
 {
-    pid_t pid = fork();
-    int status;
-    if (_strcmp(command, "exit") == 0)
-    {
-        handle_exit(args, exit_status);
-    }
-    if (pid < 0)
-    {
-        perror("fork failed");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        char command_path[256];
-        snprintf(command_path, 256, "/usr/bin/%s", args[0]);
-        execve(command_path, args, environ);
-        perror("execve failed");
-        exit(1);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+pid_t pid = fork();
+int status;
+if (_strcmp(command, "exit") == 0)
+{
+_exitt(args, exit_status);
+}
+if (pid < 0)
+{
+perror("fork failed");
+exit(1);
+}
+else if (pid == 0)
+{
+char command_path[256];
+snprintf(command_path, 256, "/usr/bin/%s", args[0]);
+execve(command_path, args, environ);
+perror("execve failed");
+exit(1);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
 }
 
 /**
@@ -102,41 +103,41 @@ void execute_command(char __attribute__((unused)) *command, char *args[], int *e
  */
 int main(void)
 {
-    char *line = NULL, *args[MAX_ARGS], previous_directory[MAX_LINE_LENGTH];
-    size_t line_length = 0;
-    ssize_t read;
-    int i, exit_status = 0;
-    previous_directory[0] = '\0';
+char *line = NULL, *args[MAX_ARGS], previous_directory[MAX_LINE_LENGTH];
+size_t line_length = 0;
+ssize_t read;
+int i, exit_status = 0;
+previous_directory[0] = '\0';
 
-    while (_printf("HOME$ "), (read = getline(&line, &line_length, stdin)) != -1)
-    {
-        i = 0;
-        args[i] = strtok(line, " \n");
-        while (args[i])
-            args[++i] = strtok(NULL, " \n");
-        if (!args[0])
-            continue;
-        if (_strcmp(args[0], "cd") == 0)
-        {
-            change_directory(args[1], previous_directory);
-            continue;
-        }
-        if (_strcmp(args[0], "exit") == 0)
-        {
-            handle_exit(args, &exit_status);
-        }
-        if (_strcmp(args[0], "env") == 0)
-        {
-            print_environment();
-            continue;
-        }
-        execute_command(args[0], args, &exit_status);
-    }
-    if (read == -1)
-    {
-        _printf("[Disconnected...]\n");
-        exit(0);
-    }
-    perror("getline failed");
-    exit(1);
+while (_printf("HOME$ "), (read = getline(&line, &line_length, stdin)) != -1)
+{
+i = 0;
+args[i] = strtok(line, " \n");
+while (args[i])
+args[++i] = strtok(NULL, " \n");
+if (!args[0])
+continue;
+if (_strcmp(args[0], "cd") == 0)
+{
+change_directory(args[1], previous_directory);
+continue;
+}
+if (_strcmp(args[0], "exit") == 0)
+{
+_exitt(args, &exit_status);
+}
+if (_strcmp(args[0], "env") == 0)
+{
+print_environment();
+continue;
+}
+execute_command(args[0], args, &exit_status);
+}
+if (read == -1)
+{
+_printf("[Disconnected...]\n");
+exit(0);
+}
+perror("getline failed");
+exit(1);
 }
