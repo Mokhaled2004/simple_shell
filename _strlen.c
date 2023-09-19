e17f1b6daac74147cf0ec94a1673c329ef3d03ad
Mokhaled2004 <mokhaled2004@gmail.com>
2023-09-19 02:34:26 -0700

    Sayonara

--- _strlen.c
+++ _strlen.c
@@ -1,19 +1,20 @@
 #include "shell.h"
 
 /**
- * custom_strlen - str len
+ * _strlen - str len
  * @str: string.
  *
  * Return: len
  */
 size_t _strlen(const char *str)
 {
-        size_t length = 0;
+size_t length = 0;
 
-        while (*str != '\0') {
-                length++;
-                str++;
-        }
+while (*str != '\0')
+{
+length++;
+str++;
+}
 
-        return length;
+return (length);
 }
