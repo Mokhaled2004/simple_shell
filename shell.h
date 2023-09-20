47829de71f7c3ec0271bb4d89fcff5b28f611e8b
Mokhaled2004 <mokhaled2004@gmail.com>
2023-09-20 05:18:51 -0700

    Sayonara

--- shell.h
+++ shell.h
@@ -2,30 +2,62 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-
-#define MAX_LINES 100
-#define MAX_LINE_LENGTH 256
-#define MAX_ARGV 10
-#define MAX_ARGS 64
-#define BUFFER_SIZE 1024
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+/* global variable to access environment */
 
 extern char **environ;
 
-void print_environmentt(void);
-void change_directory(char *directory, char *previous_directory);
-void execute_command(char *command, char *args[], int *exit_status);
-void *_realloc(void *ptr, size_t old_size, size_t new_size);
-ssize_t _getline(char **line, size_t *line_length, FILE *stream);
-void _printf(char *str);
-int _putchar(char c);
-int _strcmp(char *s1, char *s2);
-int _snprintf(char *str, size_t size, char *str1, char *str2);
-size_t _strlen(const char *str);
+int infinite_loop(char *program_path);
+
+
+void sh(char *str, char **args, char **paths, int p_cnt,
+char *p_path,  int *st);
+
+int _full_path(char *argument);
+char *_small_path(char *argument, char **path);
+
+int _strlen(char *str);
+int _strcmp(char *str1, char *str2);
+char *_strccat(char *str1, char *str2);
+char *_strcp(char *str);
+void _rcomment(char *str);
+
+char **split(char *string, char *breaks);
 int _atoi(char *s);
+char *_clStr(char *str);
+ssize_t _getline(char **string, size_t *string_size);
+
+char *_strccat2(char *str1, char *str2);
+
+char **_getallpaths(void);
+char *_getenv(char *string);
+
+void _before_exec(char **args, char **paths, int p_cnt, char *p_path, int *st);
+void _execution(char **arguments, int *st);
+
+char ***_strings(char **str);
+void _array_of_pointers(char **array);
+void _idx_from_array(int idx, char **array);
+
+int _exit(char **args, char *str, char **paths, int *st, char *pth, int p);
+int _builtin(char **arguments);
+
+int _variable(char **arguments);
+int _env_variable(char **arguments);
+int _cd(char **arguments);
+int _env(char **arguments);
+
+void _all(char **arguments, char *string, char **paths);
+
+void execve_error(char *msg, char *command, int count, char *p_path);
+/*number helper functions*/
+void _num(unsigned int n);
+int _postive(char *str);
+
 #endif
