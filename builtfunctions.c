2babb8a9f8f7a59de6d92eab882fbb53c88cee61
mazenwaelx <mazenwael575@gmail.com>
2024-10-28 08:59:18 -0700

    lol

--- builtfunctions.c
+++ builtfunctions.c
@@ -39,44 +39,45 @@ return (EXIT_SUCCESS);
 * @arguments: splitted string acc to breaks
 * Return: EXIT_SUCCESS = 0
 */
-int _cd(char **arguments)
-{
-char *pwd = NULL;
-char *opwd = NULL;
 
+int _cd(char **arguments) {
+    char *pwd = NULL;
+    char *opwd = NULL;
 
-if (arguments[1] == NULL)
-{
-arguments[1] = _getenv("HOME");
+    // Default to HOME if no directory is provided
+    if (arguments[1] == NULL) {
+        arguments[1] = _getenv("HOME");
+    }
+
+    // Check for '-' to move to OLDPWD
+    if (arguments[1][0] == '-' && arguments[1][1] == '\0') {
+        arguments[1] = _getenv("OLDPWD");
+        if (!arguments[1]) {
+            write(STDERR_FILENO, "cd: OLDPWD not set\n", 19);
+            return (EXIT_FAILURE);
+        }
+    }
+
+    // Set OLDPWD to current PWD before changing directory
+    opwd = _getenv("PWD");
+    if (opwd) {
+        _setenv("OLDPWD", opwd);
+    }
+
+    // Attempt to change directory
+    if (chdir(arguments[1]) != 0) {
+        perror("cd");
+        return (EXIT_FAILURE);
+    }
+
+    // Get new working directory and set it to PWD
+    pwd = getcwd(NULL, 0);
+    if (pwd != NULL) {
+        _setenv("PWD", pwd);
+        free(pwd);  // Safely free the memory allocated by getcwd
+        return (EXIT_SUCCESS);
+    }
+
+    return (EXIT_FAILURE);
 }
 
-
-if (arguments[1][0] == '-' && arguments[1][1] == '\0')
-{
-arguments[1] = _getenv("OLDPWD");
-if (!arguments[1])
-{
-write(STDERR_FILENO, &"cd: OLDPWD not set\n", 19);
-return (EXIT_FAILURE);
-}
-}
-opwd = _getenv("PWD");
-_setenv("OLDPWD", opwd);
-if (chdir(arguments[1]) != 0)
-{
-perror("cd");
-return (EXIT_FAILURE);
-}
-else
-{
-
-pwd = getcwd(NULL, 0);
-if (pwd != NULL)
-{
-_setenv("PWD", pwd);
-free(pwd);
-return (EXIT_SUCCESS);
-}
-}
-return (EXIT_SUCCESS);
-}
